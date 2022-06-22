d0ff1e65c7d9f795fa8b1d954ad1140db6a24af6
Vlad <billymosh@gmail.com>
2022-06-22 17:26:34 +0200

    deleted future function

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
 
-const genArr = (length , maxNumber) => {
-    const numbers = Array(length).map(()=> Math.round(Math.random()*max))
-}
+
 
 
 function PercentOfNumbers() {
-    const numbers = [...new Array(1000)].map(()=> Math.round(Math.random()*5))
-    console.log (numbers)
-    let counters = {}
+    const numbers = [...new Array(1000)].map(() => Math.round(Math.random() * 5))
+    console.log(numbers)
+    const counters = {}
 
     for (let num of numbers) {
-        if (counters[num] === undefined){
+        if (counters[num] === undefined) {
             counters[num] = 1
         } else {
             counters[num]++;
@@ -19,12 +17,10 @@ function PercentOfNumbers() {
     console.log(counters)
 
     const percents = {}
-
     for (let count in counters) {
         percents[count] = counters[count] / 1000 * 100 + "%"
     }
     console.log(percents)
-
 
 }
 PercentOfNumbers()
