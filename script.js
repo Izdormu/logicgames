ccf91fb499c3e3cba534206280ac9e517587b843
Vlad <billymosh@gmail.com>
2022-06-22 16:44:05 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+
+const genArr = (length , maxNumber) => {
+    const numbers = Array(length).map(()=> Math.round(Math.random()*max))
+}
+
+
+function PercentNumbers() {
+    const numbers = [Array(1000)].map(()=> Math.round(Math.random()*5))
+    console.log (numbers)
+
+
+    
+
+
+
+
+}
+
+PercentNumbers()
+
+
+
+
+
