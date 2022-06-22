b316bef9f658aaf26d5884332fc0b0a2b9fab6ad
Vlad <billymosh@gmail.com>
2022-06-22 17:24:49 +0200

    something

--- script.js
+++ script.js
@@ -4,19 +4,30 @@ const genArr = (length , maxNumber) => {
 }
 
 
-function PercentNumbers() {
-    const numbers = [Array(1000)].map(()=> Math.round(Math.random()*5))
+function PercentOfNumbers() {
+    const numbers = [...new Array(1000)].map(()=> Math.round(Math.random()*5))
     console.log (numbers)
+    let counters = {}
 
+    for (let num of numbers) {
+        if (counters[num] === undefined){
+            counters[num] = 1
+        } else {
+            counters[num]++;
+        }
+    }
+    console.log(counters)
 
-    
+    const percents = {}
 
-
+    for (let count in counters) {
+        percents[count] = counters[count] / 1000 * 100 + "%"
+    }
+    console.log(percents)
 
 
 }
-
-PercentNumbers()
+PercentOfNumbers()
 
 
 
