72d66272ccbe51a760cc4c194765ed49fdaf666b
Vlad <billymosh@gmail.com>
2022-06-25 04:43:06 +0200

    finished  'wtf' test about random numbers

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-
-
-
-
-function PercentOfNumbers() {
-    const numbers = [...new Array(1000)].map(() => Math.round(Math.random() * 5))
-    console.log(numbers)
+function PercentOfNumbers(numofarr,maxnum) {
+    const arrWinners = []
+    for( i=1 ;i<1000 ;i++){
+    const numbers = [...new Array(numofarr)].map(() => Math.round(Math.random() * maxnum))
+    // console.log(numbers)
     const counters = {}
 
     for (let num of numbers) {
@@ -14,18 +12,48 @@ function PercentOfNumbers() {
             counters[num]++;
         }
     }
-    console.log(counters)
+    // console.log(counters)
 
     const percents = {}
     for (let count in counters) {
-        percents[count] = counters[count] / 1000 * 100 + "%"
+        percents[count] = counters[count] / numofarr * 100 + "%"
     }
-    console.log(percents)
+    // console.log(percents)
+
+    let max = 0;
+    let maxKey = "";
+    
+    for(let count in counters){
+      if(counters[count]> max){
+        max = counters[count]
+        maxKey= count
+      
+      }
+    } 
+    // console.log(maxKey)
+    // console.log(max)
+    
+    
+    arrWinners.push(maxKey)
+
+    }
+    winners = arrWinners.map(Number)
+    winner = Math.max.apply(Math,winners)
+    console.log(arrWinners)
+    console.log(winner)
+
+
+//   for( i=1 ;i<10 ;i++){
+//    firstwin = 0
+//    keys = Object.keys(counters)
+//    if (counters[0] > counters[1]){
+    
+//     console.log("0 win")
+//    }else{
+//     console.log("1 win")
+//    }
+//    console.log(firstwin)
+//   }
 
 }
-PercentOfNumbers()
-
-
-
-
-
+PercentOfNumbers(10000,100)
